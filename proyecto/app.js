0d86820241a861b43c5b11815b12e1f92aa64b54
Juan Manuel Rodriguez <rodriguezjuanmanuelar@gmail.com>
2017-02-13 12:45:51 -0300

    rutas modulares

--- proyecto/app.js
+++ proyecto/app.js
@@ -5,12 +5,13 @@ var express=require("express");
 var bodyParser=require("body-parser");
 var User = require("./models/user").User;
 var session = require("express-session");
-
+var router_app = require("./app/router_app");
 var app= express();
 
 app.use("/public",express.static('public'));
 app.use(bodyParser.json());//para peticiones  application/json
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(session({
     secret:"123saraseando43ube",
     resave: false,
@@ -61,5 +62,5 @@ app.post("/sessions",function (req,res) {
     })
 });
 
-
+app.use("/app",router_app);
 app.listen(8080);
---
+++ proyecto/route_app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by juanma on 13/02/17.
+ */
+var express=require("express");
+
+var router = express.Router();
+
+// app.com/app/
+router.get("/", function (req, res) {
+    /* Buscar el usuario */
+    res.render("app/home")
+});
+
+module.exports = router;
