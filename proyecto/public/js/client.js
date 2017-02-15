e5258fccb208039e5802c3121c17c3376b6e5dc2
Juan Manuel Rodriguez <rodriguezjuanmanuelar@gmail.com>
2017-02-15 12:02:11 -0300

    Usando Handlebars

--- proyecto/public/js/client.js
+++ proyecto/public/js/client.js
@@ -6,4 +6,9 @@ var socket = io();
 socket.on("new image",function (data) {
     dato= JSON.parse(data);
     console.log(dato);
+    var container= document.querySelector("#imagenes");
+    var source = document.querySelector("#image-template").innerHTML;
+    var template = Handlebars.compile(source);
+
+    container.innerHTML= template(dato)+container.innerHTML;
 });
